e35e866d7dcc621261e8a50274e52f0c56ce3e95
Antoine POUJAUD <ffk30@ffk.local>
2021-01-15 15:11:44 +0100

    prout

--- main.js
+++ main.js
@@ -4,3 +4,54 @@ const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+    constructor(field) {
+        this._field = field;
+    }
+
+    print() {
+        for (let i = 0; i < this._field.length; i++)
+        {
+            this._field[i].join("");
+        }
+    }
+
+    checkPosition() {
+
+    }
+
+    move(input){
+        switch(input) {
+            case 'u' || 'U':
+
+                break;
+
+            case 'r' || 'R':
+                
+                break;
+
+            case 'd' || 'D':
+                
+                break;
+
+            case 'l' || 'L':
+                
+                break;
+        }
+       
+    }
+
+} 
+
+
+//Test 
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+  ]);
+
+  myField.print();
+  let guess = prompt('Tu vas où wallah ? ');
+  
